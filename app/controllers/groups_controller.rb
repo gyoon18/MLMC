1f6d457fc5f2c29667d464e456084941eeda3f85
raqueljoseph <raquelajoseph@gmail.com>
2021-06-22 15:37:29 -0400

    create group input

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,26 @@
 class GroupsController < ApplicationController
+  def index
+  end
+
+  def new
+      @group = Group.new
+  end
+
+  def create
+      @group = Group.new(group_params)
+      @group.save
+      redirect_to '/dashboard'
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def group_params
+      params.require(:group).permit(:name,:mentor,:date,:time,:location,:description)
+  end
 end
